0821104beb0bb8dc172ce2f844b19e8bb00df398
div910 <32384044+div910@users.noreply.github.com>
2024-12-14 03:23:36 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+let year = document.querySelector("2024");
+
+$(document).ready(function () {
+  year.innerText = new Date().getFullYear();
+});
+
 function toggleMobileMenu(){
 	document.getElementById("menu").classList.toggle("active");
 }
